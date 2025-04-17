756a5338d4e4ad3dcb1f9d1c7f3ee69152966644
Starel <aji220@naver.com>
2025-04-17 13:22:30 +0900

    feature/login

--- src/main/java/com/example/capstone/controller/MemberController.java
+++ src/main/java/com/example/capstone/controller/MemberController.java
@@ -10,7 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/members") // ✅ 변경된 경로
+@RequestMapping("/api/members") // 변경된 경로
 @RequiredArgsConstructor
 public class MemberController {
 
